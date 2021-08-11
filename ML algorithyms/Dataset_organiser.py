423ff7474da342af458bae6cd526fa8c705fbf66
Mfundo Sindane <mfundosindane0@gmail.com>
2021-08-11 20:01:28 +0200

    Update Dataset_organiser.py

--- ML algorithyms/Dataset_organiser.py
+++ ML algorithyms/Dataset_organiser.py
@@ -2,14 +2,14 @@ import pandas as pd	#Imports calculation properties
 import numpy as np	#Imports calculation with arrays properties
 import matplotlib.pyplot as plt #Imports graph plotting properties
 import seaborn as sns  #Imports graph plotting properties
-path='../input/netflix-shows/netflix_titles.csv'
+path= "Enter Path to CSV file"
 
 plt.rcParams['figure.figsize'] = [8,5]
 plt.rcParams['font.size'] =14
 plt.rcParams['font.weight']= 'bold'
 plt.style.use('seaborn-whitegrid')
 
-df =pd.read_csv(path)
+df= pd.read_csv(path)
 print("Number of dataset: ",df.shape)
 print('')
 df.head()
