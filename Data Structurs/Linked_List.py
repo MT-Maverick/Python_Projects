5dfe0e91cb3968afd7d90f2654e1132a04614e5f
Mfundo Sindane <mfundosindane0@gmail.com>
2022-01-26 23:10:08 +0200

    Update Linked_List.py
    Included Remove method in linked list class

--- Data Structurs/Linked_List.py
+++ Data Structurs/Linked_List.py
@@ -1,25 +1,27 @@
-#The Node used as a template for every node in the list:
+#The Node template:
 class Node:
     def __init__(self,data=None):
         self.data=data
         self.link=None
 
-#The Blueprint for linked-lists capabilities:
+#The linked-lists template:
 class List:
     def __init__(self):
         self.head=None
-    #The print function used to displayes every element in the linked list:
+    # Method to displayes every element in the linked list:
     def printer(self):
         printer=self.head
         while self.head !=None:
             print(printer.data)
             printer=printer.link
-    #The function used to insert a variable at the begining of the linked list:
+
+    # Method to insert at the begining of the linked list:
     def AtBegining(self,NewData):
         Nnode= Node(NewData)
         Nnode.link = self.head
         self.head =Nnode
-    #The function used to set a node at the end of the list:
+    
+    # Method used to insert a node at the end of the list:
     def AtEnd(self,NewData):
         Nnode=Node(NewData)
         if self.head ==None:
@@ -29,7 +31,8 @@ class List:
         while(last.link):
             last =last.link
         last.link=Nnode
-    
+
+    # Method used to insert a node between specified location within the list:
     def InBetween(self,Position,NewData):
         if Position == None:
             print("NODE NOT FOUND")
@@ -37,25 +40,43 @@ class List:
         Nnode =Node(NewData)
         Nnode.link = Position.link
         Position.link = Nnode
+    
+    # Method used to remove a node within the list:
+    def Remove(self,key_value):
+        head = self.head
+        if(head!=None):
+            if(head.data==key_value):
+                self.head = head.link
+                head=None
+                return
+            while(head!=None):
+                if(head.data==key_value):
+                    break
+                before =head
+                head =head.link
+            if(head==None):
+                return
+            before.link =head.link
+            head=None
 
 
-
-#The instanciation of every node in the list:
+#The instanciation the list:
 list_item = List()
-list_item.head =Node("Hello")
-list_item0 =Node("I made this")
 
-#The linkage of every node to its respective link:
-list_item.head.link =list_item0
+list_item.head =Node("Hello") 
 
-#Method to inserted node AtBegining: 
 list_item.AtBegining("Created By Mfundo Sindane:")
 
-#Method to inserted node AtEnd: 
-list_item.AtEnd("You can too")
+list_item.AtEnd("Let's Breakdown my name:")
+list_item.AtEnd("M-Motivated")
+list_item.AtEnd("F-Focused")
+list_item.AtEnd("U-Unique")
+list_item.AtEnd("N-Negative") # Removed Word
+list_item.AtEnd("N-New")
+list_item.AtEnd("D-Driven")
+list_item.AtEnd("O-Original")
 
-#If the node is inserted inbetween: 
-list_item.InBetween(list_item.head,"----------------------------------")
+list_item.Remove("N-Negative") 
+list_item.InBetween(list_item.head.link.link,"----------------------------------")
 
-#the iteration of every node:
 list_item.printer()
